46675b9e61a9cf3c4412976886457ff51bd48d3f
manasesortez <manasesortez1234@hotmail.com>
2021-09-25 11:42:44 -0600

    Guia2.1

---
+++ app/src/main/java/com/amtodev/calculator_metric/Calculator.java
@@ -0,0 +1,24 @@
+package com.amtodev.calculator_metric;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+
+        }
+    }
+
+
