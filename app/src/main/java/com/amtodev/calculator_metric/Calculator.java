40fca88bb7cb584dcd092fa57ab1e8e0c871d5aa
manasesortez <manasesortez1234@hotmail.com>
2021-09-25 15:48:56 -0600

    guia2.1

--- app/src/main/java/com/amtodev/calculator_metric/Calculator.java
+++ app/src/main/java/com/amtodev/calculator_metric/Calculator.java
@@ -3,6 +3,8 @@ package com.amtodev.calculator_metric;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,11 +14,41 @@ import android.widget.Button;
 
 public class Calculator extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    // Declare an adapter
+    RecyclerView.Adapter programAdapter;
+    RecyclerView.LayoutManager layoutmanager;
+    // Next, prepare your data set. Create two string arrays for program name and program description respectively.
+    String[] programNameList = {"C", "C++", "Java", "Android", "HTML5", "CSS3", "JavaScript", "jQuery", "Bootstrap"};
+
+    String[] programDescriptionList = {"C Description", "C++ Description", "Java Description",
+            "Android Description", "HTML5 Description",
+            "CSS3 Description", "JavaScript Description", "jQuery Description",
+            "Bootstrap Description"};
+    // Define an integer array to hold the image recourse ids
+    int[] programImages = {R.drawable.c, R.drawable.cplusplus,
+            R.drawable.java, R.drawable.android, R.drawable.html5,
+            R.drawable.css3, R.drawable.javascript, R.drawable.jquery,
+            R.drawable.bootstrap};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
+        // Obtain a handle for the RecyclerView
+        recyclerView = findViewById(R.id.rvProgram);
+        // You may use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        // Use a linear layout manager
+        layoutmanager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutmanager);
+        // Create an instance of ProgramAdapter. Pass context and all the array elements to the constructor
+        programAdapter = new ProgramAdapter(this, programNameList, programDescriptionList, programImages);
+        // Finally, attach the adapter with the RecyclerView
+        recyclerView.setAdapter(programAdapter);
 
         }
     }
